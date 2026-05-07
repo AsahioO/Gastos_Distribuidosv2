4e9f2fb32c20957014facb89725eeb6d99f465ee
AsahioO <perezreal.david@utacapulco.edu.mx>
2026-05-07 00:40:21 -0600

    feat: agregar configuración de producción y script de construcción para Render

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = list(SHARED_APPS) + [app for app in TENANT_APPS if app not in S
 MIDDLEWARE = [
     'django_tenants.middleware.main.TenantMainMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
